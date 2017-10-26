6140691422633037bc78415c6fa619755ffa1c5c
student <student@ftn.ns>
2017-10-26 15:12:02 +0200

    Dodati modeli

---
+++ Model/Akcija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POP_SF_63_2017.Model
+{
+    public class Akcija
+    {
+    public int Id { get; set; }
+
+    public bool Obrisan { get; set; }
+
+    public DateTime DatumPocetka { get; set; }
+    public DateTime DatumZavrsetka { get; set; }
+    public decimal Popust { get; set; }
+
+}
+}
---
+++ Model/Korisnik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POP_SF_63_2017.Model
+{
+    public enum TipKorisnika
+    {
+        Administrator,
+        Prodavac
+    }
+    class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public TipKorisnika TipKorisnika { get; set; }
+    }
+}
